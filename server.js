130b4158ed40fc7aee4b13439309ba36ed14688e
7c00ls <arshadshaikh4243@gmail.com>
2025-06-15 12:33:23 +0530

    server.js

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
-const express = require("express");
-const http = require("http");
-const socketIO = require("socket.io");
-
+const express = require('express');
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server, {
-  cors: {
-    origin: "*"
-  }
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', function (socket) {
+    socket.on('create', function (callback) {
+        console.log('caller at', socket.id);
+        callback(socket.id);
+    });
+
+    socket.on('join', function (code) {
+        console.log('receiver joined', socket.id, 'sending join status to', code);
+        io.to(code).emit('ready', socket.id);
+    });
+
+    socket.on('candidate', function (event) {
+        console.log('sending candiadte to', event.sendTo);
+        io.to(event.sendTo).emit('candidate', event);
+    });
+
+    socket.on('offer', function (event) {
+        console.log('sending offer to', event.receiver);
+        io.to(event.receiver).emit('offer', { event: event.sdp, caller: socket.id });
+    });
+
+    socket.on('answer', function (event) {
+        console.log('sending answer to', event.caller);
+        io.to(event.caller).emit('answer', event.sdp);
+    });
+
 });
 
-io.on("connection", socket => {
-  socket.on("offer", data => socket.broadcast.emit("offer", data));
-  socket.on("answer", data => socket.broadcast.emit("answer", data));
-  socket.on("ice-candidate", data => socket.broadcast.emit("ice-candidate", data));
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
-
-server.listen(3000, () => console.log("Signaling server running on http://localhost:3000"));
